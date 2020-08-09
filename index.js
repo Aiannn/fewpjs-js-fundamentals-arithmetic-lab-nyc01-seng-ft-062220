c1c0e9d99b3932c773a39440f38e151d27c3b2eb
Aiannn <soktoevayan@gmail.com>
2020-08-09 15:13:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 62/62
+let num2 = 62
+
+let multiply = num1*num2
+
+let random = Math.ceil(Math.random()*10)
+
+
+
+let mod = 10%6
+
+let max = Math.max(1,2,3,4,20)
